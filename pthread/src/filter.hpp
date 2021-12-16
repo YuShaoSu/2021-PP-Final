a50cb04ae73a8763e8709ade799522a238bac4ac
yussu016 <shao.cs09g@nctu.edu.tw>
2021-12-16 17:19:51 +0800

    Feat/ add pthread basic version, elapsed time calculation

---
+++ pthread/src/filter.hpp
@@ -0,0 +1,42 @@
+#ifndef FILTER_HPP
+#define FILTER_HPP
+
+#include <iostream>
+#include <cmath>
+#define PI 3.14159265
+
+using namespace std;
+
+class GaussianKernel
+{
+public:
+	int radius;
+	int dim;
+	double **matrix;
+	double sigma;
+	double sum = 0.0;
+	// thread variable
+	int t_num, per_t, t_remain;
+	GaussianKernel(const int r, const double s, const int t_num):radius(r), sigma(s), t_num(t_num)
+	{
+		dim = 2 * radius + 1;
+		matrix = new double* [dim];
+		for(int i = 0; i < dim; i++)
+			matrix[i] = new double [dim];
+		per_t = radius*2 / t_num;
+		t_remain = (radius*2) % t_num;
+	}
+	~GaussianKernel()
+	{
+		cout << "call of GaussianKernel destructor" << endl;
+		for(int i = 0; i < dim ; i++)
+			delete [] matrix[i];
+		delete [] matrix;
+	}
+	void Calculation();
+	void CalculationWorker(int idx);
+};
+
+double gauss_2d(const int x,  const int y,  const double sigma);
+
+#endif //FILTER_H
--- pthread/src/main.cpp
+++ pthread/src/main.cpp
@@ -1,30 +1,29 @@
 #include "image.hpp"
 #include <iostream>
-
+#include <string>
 #include "../../common/CycleTimer.h"
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
 
+	if (argc != 2) {
+		printf("Please give the number of threads!\n");
+		return 0;
+	}
+
 	// Original image file name
 	string image_file_name= "../../image/big.jpg", result_file_name= "filter.jpg";
 	int radius = 7;
 	double sigma = 10;
+	int t_num = stoi(argv[1]);
 
-	/*cout << "Original image name: " << endl;
-	cin >> image_file_name;
-	
-	cout << "Result image name: " << endl;
-	cin >> result_file_name;
-    
-	cout << "Radius: " << endl;  
-	cin >> radius;
-	
-	cout << "Sigma:" << endl;
-	cin >> sigma;*/
 
-	GaussianKernel kernel(radius,sigma);
+    // Image img(image_file_name, result_file_name, t_num);
+	// img.image_load();
+
+
+	GaussianKernel kernel(radius,sigma,t_num);
 	double startTime = CycleTimer::currentSeconds();
 	kernel.Calculation();
 	double endTime = CycleTimer::currentSeconds();
@@ -33,7 +32,7 @@ int main(int argc, char *argv[]) {
 
 	cout << "Loading image..." << endl;
 	
-    Image img(image_file_name, result_file_name);
+    Image img(image_file_name, result_file_name,t_num,kernel);
 
 	img.image_load();
 
@@ -42,7 +41,8 @@ int main(int argc, char *argv[]) {
 	cout << "Appling filter..." << endl;
 
 	startTime = CycleTimer::currentSeconds();
-	img.GaussianFliter(kernel);
+	// img.GaussianFilter(kernel);
+	img.GaussianFilter();
 	endTime = CycleTimer::currentSeconds();
 	cout << "Apply filter elapsed time: " << endTime - startTime << endl;
 
